ff62c108b2d1872c9b2682735775e9aba704800a
VocaloidZero <341151@colum.edu>
2020-11-23 10:41:07 -0500

    Player Movement

---
+++ SheepCount/Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 direction;
+    public float speed;
+    public float jumpForce;
+    private bool facingRight = false;
+
+    //Check if on ground
+    private bool onGround;
+    public Transform groundCheck;
+    public float groundRadius;
+    public LayerMask whatIsGround;
+
+    PlayerController playerController;
+    private Rigidbody2D rb2D;
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        if (playerController == null)
+        {
+            playerController = this.gameObject.AddComponent<PlayerController>();
+        }
+    }
+    private void FixedUpdate()
+    {
+        //Update Player position
+        UpdateInput();
+        rb2D.MovePosition(rb2D.position + direction * speed * Time.fixedDeltaTime);
+
+        //ground check
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        //Flip player
+        if (facingRight == false && playerController.keyDirection.x > 0 || facingRight == true && playerController.keyDirection.x < 0)
+        {
+            Flip();
+        }
+
+    }
+    void Update()
+    {
+        Jump();
+    }
+    protected virtual void UpdateInput()
+    {
+        if (playerController.IsKeyDown)
+        {
+            this.direction = playerController.direction;
+        }
+        else
+        {
+            this.direction = Vector3.zero;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    void Jump()
+    {
+
+    }
+}
--- SheepCount/Assets/Scripts/PlayerController.cs
+++ SheepCount/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
 
     public Vector2 direction = new Vector2();
     public Vector2 keyDirection;
@@ -15,11 +17,10 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public PlayerController()
+    void Start()
     {
-
+       // rb = GetComponent<Rigidbody2D>();
     }
-
     void Awake()
     {
         AwakeKeyDirection();
@@ -29,23 +30,23 @@ public class PlayerController : MonoBehaviour
     {
         keyDirection = new Vector2();
     }
-
-    // Use this for initialization
-    void Start()
+    void FixedUpdate()
     {
+        UpdateMovement();
 
     }
 
-    // Update is called once per frame
-    protected virtual void Update()
+    private void Update()
     {
-        UpdateKeyboardInput();
-
     }
 
-    protected virtual void UpdateKeyboardInput()
+    protected virtual void UpdateMovement()
     {
-        //direction.x = direction.y = 0;
+
+        //Movement
+        //moveInput = Input.GetAxis("Horizontal");
+        //rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
         keyDirection.x = keyDirection.y = 0;
 
         //Keyboard
@@ -57,21 +58,33 @@ public class PlayerController : MonoBehaviour
         {
             keyDirection.x += -1;
         }
-
         if (Input.GetKey("up"))
         {
-            keyDirection.y += 0;
-
-        }
-        if (Input.GetKey("down"))
-        {
-            keyDirection.y += 0;
+           keyDirection.y += 1;
         }
 
-
+        //Flip player
+        //if (facingRight == false && keyDirection.y > 0 || facingRight == true && keyDirection.y < 0)
+        //{
+        //    Flip();
+        //}
 
         direction += keyDirection;
         direction.Normalize();
     }
-}
 
+    //void Jump()
+    //{
+    //    if (Input.GetKey("up"))
+    //    {
+    //        keyDirection.y += 1;
+    //    }
+    //}
+    //void Flip()
+    //{
+    //    facingRight = !facingRight;
+    //    Vector3 Scaler = transform.localScale;
+    //    Scaler.x *= -1;
+    //    transform.localScale = Scaler;
+    //}
+}
